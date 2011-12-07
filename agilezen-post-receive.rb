3a79b41498768b98dddf4e390ffb0898df7ea38b
Stian Mathiassen <stian.mathiassen@bekk.no>
2011-12-07 14:55:03 +0100

    Added logging of AG path

--- agilezen-post-receive.rb
+++ agilezen-post-receive.rb
@@ -26,6 +26,7 @@ post "/" do
     data = "{\"text\":\"#{comment_message_from commit }\"}"
     commit['message'].scan(task_id_regex).each do |story_id|
       path = "https://agilezen.com/api/v1/projects/#{params[:board_id]}/stories/#{story_id}/comments"
+      puts path
       puts http.post path,data, headers 
     end
   end
